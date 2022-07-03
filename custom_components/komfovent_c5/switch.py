6c279629815965b515679d188e40863dacde6a27
Simon Berger <simon@siku2.io>
2022-07-03 23:32:46 +0200

    cache switch state

--- custom_components/komfovent_c5/switch.py
+++ custom_components/komfovent_c5/switch.py
@@ -1,4 +1,5 @@
-from typing import Any
+import time
+from typing import Any, Optional
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
@@ -22,24 +23,66 @@ async def async_setup_entry(
     return True
 
 
-class AhuControl(KomfoventEntity, SwitchEntity):
+class CachedKomfoventSwitch(KomfoventEntity, SwitchEntity):
+    CACHE_EXPIRATION = 30
+
+    _cached_is_on: Optional[bool]
+    _cached_is_on_ts: Optional[float]
+
+    def __init__(self, coordinator: KomfoventCoordinator) -> None:
+        super().__init__(coordinator)
+        self._cached_is_on = None
+        self._cached_is_on_ts = None
+
+    @property
+    def is_on(self) -> bool:
+        if cached := self.cached_is_on is not None:
+            return cached
+        return self.uncached_is_on
+
+    @property
+    def cached_is_on(self) -> Optional[bool]:
+        if self._cached_is_on is None or self._cached_is_on_ts is None:
+            return None
+        diff = time.time() - self._cached_is_on_ts
+        if diff >= self.CACHE_EXPIRATION:
+            # reset because cache expired
+            self._cached_is_on = None
+            self._cached_is_on_ts = None
+        return self._cached_is_on
+
+    @property
+    def uncached_is_on(self) -> bool:
+        raise NotImplementedError()
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        self._cached_is_on = True
+        self._cached_is_on_ts = time.time()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        self._cached_is_on = False
+        self._cached_is_on_ts = time.time()
+
+
+class AhuControl(CachedKomfoventSwitch):
     @property
     def name(self) -> str:
         return f"{super().name} Ahu Control"
 
-    # TODO icon
     @property
-    def is_on(self) -> bool:
+    def uncached_is_on(self) -> bool:
         return self._modes_state.ahu
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         await self._modes_client.set_ahu_on(True)
+        await super().async_turn_on(**kwargs)
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         await self._modes_client.set_ahu_on(False)
+        await super().async_turn_off(**kwargs)
 
 
-class OcvControl(KomfoventEntity, SwitchEntity):
+class OcvControl(CachedKomfoventSwitch):
     @property
     def name(self) -> str:
         return f"{super().name} Ocv Control"
@@ -49,11 +92,13 @@ class OcvControl(KomfoventEntity, SwitchEntity):
         return EntityCategory.CONFIG
 
     @property
-    def is_on(self) -> bool:
+    def uncached_is_on(self) -> bool:
         return self._functions_state.ocv_enabled
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         await self._functions_client.set_ocv_enabled(True)
+        await super().async_turn_on(**kwargs)
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         await self._functions_client.set_ocv_enabled(False)
+        await super().async_turn_off(**kwargs)
